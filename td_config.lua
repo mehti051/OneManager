09cfa7300106821890f5f69a629818628a817b10
MaxTeam-co <mosydev2017@gmail.com>
2017-01-19 15:14:02 +0330

    Add files via upload

---
+++ td_config.lua
@@ -0,0 +1,13 @@
+do local _ = {
+  help_text = "*Help Text For Your Group\n----------------------\nبرای قفل کردن چیزی از دستور زیر استفاده کنید\nlock [edit|reply|forward|arabic|english|flood|links]\nو برای باز کردن یکی از تنظیات بالا از دتستور زیر استفاده کنید\nunlock [edit|reply|forward|arabic|english|flood|links]\nبرای میوت کردن عکس،گیف و غیر از دستور زیر استفاده کنید\nmute [all|gif|document|contact|video|audio|location|sticker|photo]\nوبرای غیر فعال کردن دستورات بالا از دستور زیر استفاده کنید\nunmute [all|gif|document|contact|video|audio|location|sticker|photo]\nبرای تنظیم کارکترای های پیام از دستور زیر استفاده کنید\nsetchare [مقدار کارکتر]\nبرای تنظیم مقدار پیام های تکراری از دستور زیر استفاده کنید\nsetflood [تعداد]\nبرای دریافت اطلاعات خود و دیگران از دستور زیر استفاده کنید\nid [ریپلی]\nبرای دریافت تنظیمات گروه از دستور زیر استفاده کنید\nsettings\nبرای دریافت بازدید یک مطلب از دستور زیر استفاده کنید\nviews\n\nشما می توانید از دستور زیر استفاده نمایید\n[#!/]*\n----------------------\nOur Channel: @MaxTeamCh",
+  sudo_users = {
+173929873,
+224976780,
+200106480,
+198794027,
+274408251,
+228347356
+  }
+}
+return _
+end
